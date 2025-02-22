fd50e2305647a85ce53a2dd3ac802d2e9e91f45d
Abril Guzman Barrera <230350@utxicotepec.edu.mx>
2025-02-22 13:18:30 -0600

    Primeros ejercicios 1-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Ejercicio01Component } from './ejercicio-01/ejercicio-01.component';
+import { Ejercicio02Component } from './ejercicio-02/ejercicio-02.component';
+import { Ejercicio03Component } from './ejercicio-03/ejercicio-03.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Ejercicio01Component, Ejercicio02Component, Ejercicio03Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/ejercicio-01/ejercicio-01.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-01',
+  imports: [],
+  template: 'Hello Universe.',
+  styles:` 
+  :host {
+    color:rgb(100, 34, 154);
+  }`
+})
+export class Ejercicio01Component {
+
+}
